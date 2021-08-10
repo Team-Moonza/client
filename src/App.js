f61bfdd0b437d24beb6a2cc39bd67f3b29d31272
humza1997 <humza786@gmail.com>
2021-08-10 15:41:51 +0100

    Foundation files created.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div id='app'>
+      <h1>hello!</h1>
+    </div>
+  );
+}
+
+export default App;
